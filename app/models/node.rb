9b845e41b8356cdc0efb7ac5963ef0a0a495e947
You Matsumoto <youmts22@gmail.com>
2019-01-06 16:41:02 +0900

    Add node model test.

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,3 @@
 class Node < ApplicationRecord
-  has_closure_tree
+  has_closure_tree order: :sort_order
 end
--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Node, type: :model do
-  it "モデルが登録できること"
+  it "rootが登録できること" do
+    node = Node.create(
+      name: "test",
+      sort_order: "0",
+    )
+    expect(node.name).to eq "test"
+  end
+
+  it "childが登録できること" do
+    root = Node.create(
+      name: "root",
+    )
+
+    expect {
+      root.children.create(name:  "child")
+    }.to change(root.children, :length).by(1)
+  end
+
+  it "childがsort_orderの順序で並ぶこと" do
+    root = Node.create(
+      name: "root",
+    )
+
+    root.children.create(name: "child1", sort_order: "1")
+    root.children.create(name: "child0", sort_order: "0")
+
+    expect(root.children[0].name).to eq "child0"
+    expect(root.children[1].name).to eq "child1"
+  end
 end
