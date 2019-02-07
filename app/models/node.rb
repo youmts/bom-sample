0bc971c5bdf35d4ebd315e288292cd5fa32ce083
You Matsumoto <youmts22@gmail.com>
2019-02-07 23:49:18 +0900

    Install annotate gem and apply

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: nodes
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  sort_order :string
+#  parent_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Node < ApplicationRecord
   has_closure_tree order: :sort_order
 end
--- spec/factories/nodes.rb
+++ spec/factories/nodes.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: nodes
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  sort_order :string
+#  parent_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :root, class: Node do
     name { "root" }
