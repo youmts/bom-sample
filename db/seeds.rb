1992617666b9b5dfc77b39d669f3f973a38ab860
You Matsumoto <youmts22@gmail.com>
2019-02-08 22:31:54 +0900

    Create big db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-root = Node.create(name: 'root')
+root = Node.create(name: 'root(3)')
 child = root.children.create(name: 'child')
 child.children.create(name: 'grandchild')
 
-root2 = Node.create(name: 'root2')
+root2 = Node.create(name: 'root(1)')
+
+root100 = Node.create(name: 'root(100)')
+9.times do |x|
+  c = root100.children.create(name: Faker::Address.country)
+  9.times do |y|
+    c.children.create(name: Faker::Address.city)
+  end
+end
+
+root1000 = Node.create(name: 'root(1000)')
+9.times do |x|
+  c = root1000.children.create(name: Faker::Address.country)
+  9.times do |y|
+    gc = c.children.create(name: Faker::Address.city)
+    9.times do |z|
+      ggc = gc.children.create(name: Faker::Address.street_address)
+    end
+  end
+end
+
+root10000 = Node.create(name: 'root(10000)')
+9.times do |x|
+  c = root1000.children.create(name: Faker::Address.country)
+  9.times do |y|
+    gc = c.children.create(name: Faker::Address.city)
+    9.times do |z|
+      ggc = gc.children.create(name: Faker::Address.street_address)
+      9.times do |u|
+        gggc = ggc.children.create(name: Faker::Name.name)
+      end
+    end
+  end
+end
