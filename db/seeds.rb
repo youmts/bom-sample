f2afc4619dc81832a40e2fa1c3b4e52d3ca2f65c
You Matsumoto <youmts22@gmail.com>
2019-02-18 21:24:47 +0900

    Reduce db size

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,17 +31,3 @@ root1000 = Node.create(name: 'root(1000)')
     end
   end
 end
-
-root10000 = Node.create(name: 'root(10000)')
-9.times do |x|
-  c = root10000.children.create(name: Faker::Address.country)
-  9.times do |y|
-    gc = c.children.create(name: Faker::Address.city)
-    9.times do |z|
-      ggc = gc.children.create(name: Faker::Address.street_address)
-      9.times do |u|
-        gggc = ggc.children.create(name: Faker::Name.name)
-      end
-    end
-  end
-end
