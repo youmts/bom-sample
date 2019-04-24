6781f6f861f42376f2cc3b5d9df2c40df707fb5e
You Matsumoto <youmts22@gmail.com>
2019-04-24 20:51:08 +0900

    Create feature spec

--- spec/features/boms_spec.rb
+++ spec/features/boms_spec.rb
@@ -7,17 +7,29 @@ RSpec.feature "Boms", type: :feature do
     @gchild = @child.children.create(name: "grand_child_node_name")
   end
 
-  xscenario "select root nodes, show bom", js: true do
+  scenario "select root nodes, show bom", js: true do
     visit root_path
 
+    # rootのみ一覧に表示されること
     expect(page).to have_content @root.name
     expect(page).not_to have_content @child.name
     expect(page).not_to have_content @gchild.name
 
-    expect(page).to have_content 'show'
-    li = find("#app #list").find(@root.name)
+    # 一覧の行を選択する
+    click_button('forward')
 
-    expect(page).to have_content @child.name
-    expect(page).to have_content @gchild.name
+    # Treeのrootノード
+    node = find('div#tree ul li')
+    expect(node).to have_content @root.name
+
+    # 子ノード
+    node.find('div.tree-item-expander').click
+    node = node.find('ul li')
+    expect(node).to have_content @child.name
+
+    # 孫ノード
+    node.find('div.tree-item-expander').click
+    node = node.find('ul li')
+    expect(node).to have_content @gchild.name
   end
 end
